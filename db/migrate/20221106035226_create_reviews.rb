124425df059cdfa812003dbf19ea23e7f00df9ca
+ <awino.millicent@student.moringaschool.com>
2022-11-06 21:26:07 +0300

    completed challenge

--- db/migrate/20221106035226_create_reviews.rb
+++
@@ -1,9 +0,0 @@
-class CreateReviews < ActiveRecord::Migration[6.1]
-  def change
-    create_tables :reviews do |t|
-      t.integer :star_rating
-      t.string  :comment
-      t.integer :product_id
-  end
-end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ Product.destroy_all
 User.destroy_all
 
 puts "Creating users..."
-user1 = User.create(name: Faker::Name.name)
-user2 = User.create(name: Faker::Name.name)
-user3 = User.create(name: Faker::Name.name)
+user1 = User.create(name: "John")
+user2 = User.create(name: "Ali")
+user3 = User.create(name: "Grace")
 
 puts "Creating products..."
 product1 = Product.create(name: "Stapler", price: 10)
@@ -22,5 +22,9 @@ puts "Creating reviews..."
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
-
+Review.create(comment: "Nice",star_rating: 10, product_id:product1.id, user_id: user1.id)
+Review.create(comment: "Wow",star_rating: 7, product_id:product2.id, user_id: user2.id)
+Review.create(comment: "Fantastic",star_rating: 5, product_id:product3.id, user_id: user3.id)
+Review.create(comment: "Tried",star_rating: 3, product_id:product4.id, user_id: user3.id)
+Review.create(comment: "Bad",star_rating: 1, product_id:product5.id, user_id: user2.id)
 puts "Seeding done!"
